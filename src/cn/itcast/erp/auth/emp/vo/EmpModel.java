1b94d27d74f5e3bf2b689fa8c6870ccdc1bc371f
yinxin <yinxin0476@vip.qq.comm>
2017-02-14 19:05:51 +0800

    ERP

--- src/cn/itcast/erp/auth/emp/vo/EmpModel.java
+++ src/cn/itcast/erp/auth/emp/vo/EmpModel.java
@@ -1,8 +1,5 @@
 package cn.itcast.erp.auth.emp.vo;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
@@ -11,23 +8,22 @@ import cn.itcast.erp.auth.dep.vo.DepModel;
 import cn.itcast.erp.auth.role.vo.RoleModel;
 import cn.itcast.erp.util.format.FormatUtil;
 
-
 public class EmpModel {
 	public static final String EMP_LOGIN_USER_OBJECT_NAME = "loginEm";
-	
-	//数据结构思想应用
+
+	// 数据结构思想应用
 	public static final Integer EMP_GENDER_OF_MAN = 1;
 	public static final Integer EMP_GENDER_OF_WOMAN = 0;
-	
+
 	public static final String EMP_GENDER_OF_MAN_VIEW = "男";
 	public static final String EMP_GENDER_OF_WOMAN_VIEW = "女";
-	
+
 	public static final Map<Integer, String> genderMap = new HashMap<Integer, String>();
-	static{
+	static {
 		genderMap.put(EMP_GENDER_OF_MAN, EMP_GENDER_OF_MAN_VIEW);
 		genderMap.put(EMP_GENDER_OF_WOMAN, EMP_GENDER_OF_WOMAN_VIEW);
 	}
-	
+
 	private Long uuid;
 	private String userName;
 	private String name;
@@ -37,39 +33,34 @@ public class EmpModel {
 	private String address;
 	private String lastLoginIp;
 	private Integer loginTimes;
-	
+
 	private Long lastLoginTime;
-	
+
 	private Integer gender;
 	/*
-	//Long:记录的是毫秒值	
-	//Date:对long的包装	优点：格式好，缺点：计算时间略有复杂性
-	现在的时间是2020年4月31日
-	180天前是几号？
-	现在的long System.currentTimeMillis()-180*24*60*60*1000
-	long-long >0
-	Date  2014年1月4日  14：21
-	Date  2014年1月4日  14：22	
-	*/
+	 * //Long:记录的是毫秒值 //Date:对long的包装 优点：格式好，缺点：计算时间略有复杂性 现在的时间是2020年4月31日
+	 * 180天前是几号？ 现在的long System.currentTimeMillis()-180*24*60*60*1000 long-long
+	 * >0 Date 2014年1月4日 14：21 Date 2014年1月4日 14：22
+	 */
 	private Long birthday;
-	
-	//视图值：视图值是一种用于界面显示的变量值，该值不具体对应某个数据库字段，它服务于某个数据库字段
-	//当数据库中的某个字段值不便于直接显示时，为该字段添加视图值，用于显示对应的信息
-	//1.定义一个String类型的变量，变量名是无法合理显示的字段的字段名+View
-	//2.提供其get方法
-	//3.在其对应的变量的set方法中对这个View值进行初始化
+
+	// 视图值：视图值是一种用于界面显示的变量值，该值不具体对应某个数据库字段，它服务于某个数据库字段
+	// 当数据库中的某个字段值不便于直接显示时，为该字段添加视图值，用于显示对应的信息
+	// 1.定义一个String类型的变量，变量名是无法合理显示的字段的字段名+View
+	// 2.提供其get方法
+	// 3.在其对应的变量的set方法中对这个View值进行初始化
 	private String birthdayView;
 	private String genderView;
 	private String lastLoginTimeView;
-	
-	//辅助值
+
+	// 辅助值
 	private String resAll;
 
-	//多对一
+	// 多对一
 	private DepModel dm;
-	//对角色多对多
+	// 对角色多对多
 	private Set<RoleModel> roles;
-	
+
 	public Set<RoleModel> getRoles() {
 		return roles;
 	}
@@ -104,6 +95,8 @@ public class EmpModel {
 		this.lastLoginTime = lastLoginTime;
 		this.lastLoginTimeView = FormatUtil.formatDate(lastLoginTime);
 	}
+
+	// 在其对应的变量的set方法中对这个View进行初始化
 	public String getGenderView() {
 		return genderView;
 	}
@@ -191,5 +184,5 @@ public class EmpModel {
 	public void setDm(DepModel dm) {
 		this.dm = dm;
 	}
-	
+
 }
--- src/cn/itcast/erp/util/listener/AllResLoadListener.java
+++ src/cn/itcast/erp/util/listener/AllResLoadListener.java
@@ -12,24 +12,26 @@ import org.springframework.web.context.support.WebApplicationContextUtils;
 import cn.itcast.erp.auth.res.business.ebi.ResEbi;
 import cn.itcast.erp.auth.res.vo.ResModel;
 
-public class AllResLoadListener implements ServletContextListener{
+public class AllResLoadListener implements ServletContextListener {
 
 	public void contextInitialized(ServletContextEvent event) {
-		//读取所有资源信息，放入SerlvetContext范围
-		//使用spring的上下文对象
+		// 读取所有资源信息，放入SerlvetContext范围
+		// 使用spring的上下文对象
+		// 使用Spring的ApplicationContext 所以监听器在web.xml配置顺序在Spring下
 		ServletContext sc = event.getServletContext();
-		WebApplicationContext ctx = WebApplicationContextUtils.getWebApplicationContext(sc); 
+		WebApplicationContext ctx = WebApplicationContextUtils
+				.getWebApplicationContext(sc);
 		ResEbi resEbi = (ResEbi) ctx.getBean("resEbi");
 		List<ResModel> resList = resEbi.getAll();
 		StringBuilder sbf = new StringBuilder();
-		for(ResModel temp :resList){
+		for (ResModel temp : resList) {
 			sbf.append(temp.getUrl());
 			sbf.append(",");
 		}
-		//放入sc中
+		// 放入sc中
 		sc.setAttribute("allRes", sbf.toString());
 	}
-	
+
 	public void contextDestroyed(ServletContextEvent arg0) {
 	}
 }
